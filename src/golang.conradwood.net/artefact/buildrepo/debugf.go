d6822f674e18c055402ba689c1e882d3dfef679e
Conrad Wood <cnw@conradwood.net>
2023-09-01 16:42:01 +0100

    automatic commit
    Change-Id: I84e1c9f7ee3d9e9100e3c48a9608459ce7f7a834

---
+++ src/golang.conradwood.net/artefact/buildrepo/debugf.go
@@ -0,0 +1,13 @@
+package buildrepo
+
+import (
+	"fmt"
+)
+
+func debugf(format string, args ...interface{}) {
+	if !*debug {
+		return
+	}
+	s := fmt.Sprintf(format, args...)
+	fmt.Print("[buildrepo] " + s)
+}
---
+++ src/golang.conradwood.net/artefact/server/debugf.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func debugf(format string, args ...interface{}) {
+	if !*debug {
+		return
+	}
+	fmt.Printf(format, args...)
+}
