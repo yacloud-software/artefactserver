85e900a3285030d9db1928cdc0e4d9e1e1c7dc70
Conrad Wood <cnw@conradwood.net>
2024-02-14 09:43:56 +0000

    automatic commit

--- src/golang.conradwood.net/artefact/server/latest_builds.go
+++ src/golang.conradwood.net/artefact/server/latest_builds.go
@@ -5,9 +5,11 @@ import (
 	pb "golang.conradwood.net/apis/artefact"
 	"golang.conradwood.net/apis/common"
 	"golang.conradwood.net/apis/gitserver"
+	"golang.conradwood.net/go-easyops/authremote"
 )
 
 func (a *artefactServer) LatestBuildForGoEasyops(ctx context.Context, req *common.Void) (*pb.LatestBuild, error) {
+	ctx = authremote.Context()
 	gr := &gitserver.ByIDRequest{ID: 59} // go-easyops is maintained in git and there it is ID 59
 	lb, err := gitserver.GetGIT2Client().GetLatestSuccessfulBuild(ctx, gr)
 	if err != nil {
